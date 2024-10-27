dd740f0a9ba86c942ca666bc71a41f9166d3b605
Alex <87208475+K0uzer@users.noreply.github.com>
2024-10-27 21:09:03 +0300

    refactor: Refactoring App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import React, {useState} from 'react';
-import './App.css';
-import Weather from './components/Weather';
-
+import React, { useState } from 'react'
+import './App.css'
+import Weather from './components/Weather'
 
 function App() {
-    const [variables, setVariables] = useState(['rain_sum', 'snowfall_sum']);
+    const [variables, setVariables] = useState([
+        'rain_sum',
+        'snowfall_sum',
+        'precipitation_hours',
+        'shortwave_radiation_sum',
+    ])
 
-  return (
-    <div className="main">
-        <div>
+    return (
+        <div className="main">
             <label>
-                {/* available values:
-                weathercode, temperature_2m_max, temperature_2m_min, apparent_temperature_max, apparent_temperature_min, sunrise, sunset, precipitation_sum, rain_sum,
-                showers_sum, snowfall_sum, precipitation_hours, windspeed_10m_max, windgusts_10m_max, winddirection_10m_dominant, shortwave_radiation_sum, et0_fao_evapotranspiration
-                */}
-
-                <input type="text" onInput={e => {
-                    variables.push((e.target as HTMLInputElement).value)
-                }}/>
-
+                <input
+                    className="main__input"
+                    type="text"
+                    placeholder="Введите данные"
+                    onInput={(e) => {
+                        variables.push((e.target as HTMLInputElement).value)
+                    }}
+                />
             </label>
+            <Weather lat={55.751244} long={37.618423} variables={variables} />
         </div>
-      <Weather lat={55.751244} long={37.618423} variables={variables} />
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
